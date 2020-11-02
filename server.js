4251655c34e8791b5f526db15facaf9bb07a126f
Marisa Raeder <mraeder11@gmail.com>
2020-11-02 17:43:31 -0600

    index

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {   // req object can be coming f
     console.log(req.headers);                     // console logging this so we can see headers 
     res.statusCode = 200;                         // everything ok 
     res.setHeader('Content-Type', 'text/html');   // tells client what kind of data to expect in response body 
-    res.end('<html><body><h1>Hello World!</h1></body></html>');  // close response body and passes message to user 
+    res.end('<html><body><h1>Welcome to the Cohub site!</h1></body></html>');  // close response body and passes message to user 
 });
 
 server.listen(port, hostname, () => {          // 2 arguments; port and hostname variables, 3rd argument is a callback function that will be executed when the server starts up
