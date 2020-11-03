14358b4a92971f37fd710ab84aa52b8fea365e70
Marisa Raeder <mraeder11@gmail.com>
2020-11-02 18:23:46 -0600

    Error callback patterns

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ const server = http.createServer((req, res) => {   // req object can be coming f
             fileUrl = '/index.html';  // ... then req.url property will just contain forward slash and we'll automatically send back index.html page 
         }
 
-        const filePath = path.resolve('./public' + fileUrl);
-        const fileExt = path.extname(filePath);
+        const filePath = path.resolve('./public' + fileUrl);  // get absolute path of file that's been requested, store it in filePath. Use path.resolve to convert from relative to absolute path. Req file is in public folder + file url
+        const fileExt = path.extname(filePath);   // server should only grab requests for HTML files. Use this line to check if HTML file
 
-        if (fileExt === '.html') {
-            fs.access(filePath, err => {
-                if (err) {
+        if (fileExt === '.html') {   // if fileExt = html, we know we have a GET request for an HTML file 
+            fs.access(filePath, err => {    // this access method takes 2 arguments, path of file we want to check and callback that takes error argument 
+                if (err) {   // if file is not accessible, then error object passed to err argument, if truthy then 404
                     res.statusCode = 404;
                     res.setHeader('Content-Type', 'text/html');
                     res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
@@ -31,10 +31,10 @@ const server = http.createServer((req, res) => {   // req object can be coming f
 
                 fs.createReadStream(filePath).pipe(res);
             });
-        } else {    
+        } else {      // if file ext is NOT html, we'll send 404 error response
             res.statusCode = 404;
             res.setHeader('Content-Type', 'text/html');
-            res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
+            res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);  // file url is not html
         }
     } else {   // this handles anything other than GET request 
         res.statusCode = 404;
