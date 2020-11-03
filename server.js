ad49e04eb1265a8bf4f1fc9391a441a5713eeffa
Marisa Raeder <mraeder11@gmail.com>
2020-11-03 14:26:21 -0600

    morgan middleware

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express'); 
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));  // morgan middleware with morgan function with argument 'dev'. Configures morgan to log using the development version which will give us additional information
 
+//set up express to serve files from the public folder using 1 line of code
+app.use(express.static(__dirname + '/public'));  // middleware func built in called express.static. Pass it express.static func w argument __dirname (special node variable = absolute path of the current directory of the file that it's in)
+ 
 // this is a middleware function 
 app.use((req, res) => {   // use method takes callback function (middleware function). Middleware in express has access to req (request object), res (response object), and next 
-    console.log(req.headers);    // console req headers
     res.statusCode = 200;        // success code 
     res.setHeader('Content-Type', 'text/html');   // content = text 
     res.end('<html><body><h1>This is an Express Server</h1></body></html>'); // end it with inline HTML doc 
