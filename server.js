afc059a927f91f5a63d60da6a7e93fce1c66ff4b
Marisa Raeder <mraeder11@gmail.com>
2020-11-04 09:31:28 -0600

    materials.html

--- server.js
+++ server.js
@@ -21,36 +21,41 @@ app.get('/materials', (req, res) => {   // get request, don't have 'next' in her
     res.end('Will send all the materials to you');  // status code and headers already set with app.all. Just end it to send message to client.
 });
 
-app.post('/campsites', (req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+// handle post request for materials path
+app.post('/materials', (req, res) => {   // if req was post request, it would go app.all to app.post, skipping app.get
+    res.end(`Will add the material: ${req.body.name} with description: ${req.body.description}`);  // body-parser takes properties from that JSON data and auto set it up as properties of the req.body object 
 });
 
-app.put('/campsites', (req, res) => {
+// handle put request for materials path - rejected 
+app.put('/materials', (req, res) => {
+    res.statusCode = 403;  // reject request to this endpoint, operation not supported
+    res.end('PUT operation not supported on /materials');
+});
+
+// handle delete operations to /materials
+app.delete('/materials', (req, res) => {  // dangerous operation, only allow for restricted users 
+    res.end('Deleting all materials');
+});
+
+// endpoints to support a different path 
+app.get('/materials/:materialId', (req, res) => {   // add route parameter to end of the path with /:materialId. Store whatever client sends as part of path after /: as a route param named materialId
+    res.end(`Will send details of the material: ${req.params.materialId} to you`);
+});
+
+app.post('/materials/:materialId', (req, res) => {
     res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
+    res.end(`POST operation not supported on /materials/${req.params.materialId}`);
 });
 
-app.delete('/campsites', (req, res) => {
-    res.end('Deleting all campsites');
+// support put requests on specific materialId
+app.put('/materials/:materialId', (req, res) => {
+    res.write(`Updating the material: ${req.params.materialId}\n`);  // \n causes new line in body
+    res.end(`Will update the material: ${req.body.name}
+        with description: ${req.body.description}`); // parsing out from request body that will be sent to us. Pulling this out of JSON formatted body of the request message, and echoing it back in our response as text 
 });
 
-app.get('/campsites/:campsiteId', (req, res) => {
-    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
-});
-
-app.post('/campsites/:campsiteId', (req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-});
-
-app.put('/campsites/:campsiteId', (req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name}
-        with description: ${req.body.description}`);
-});
-
-app.delete('/campsites/:campsiteId', (req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+app.delete('/materials/:materialId', (req, res) => {
+    res.end(`Deleting material: ${req.params.materialId}`);  // This endpoint used for deleting specific material, not all of them 
 });
 
 //set up express to serve files from the public folder using 1 line of code. This is how we serve up About and Home!
@@ -60,7 +65,7 @@ app.use(express.static(__dirname + '/public'));  // middleware func built in cal
 app.use((req, res) => {   // use method takes callback function (middleware function). Middleware in express has access to req (request object), res (response object), and next 
     res.statusCode = 200;        // success code 
     res.setHeader('Content-Type', 'text/html');   // content = text 
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>'); // end it with inline HTML doc 
+    res.end('<html><body><h1>This is the Materials Hub server</h1></body></html>'); // end it with inline HTML doc 
 });
 
 // create a server and start listening to it 
